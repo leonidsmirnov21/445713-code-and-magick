77a3244addb8ca861bbdd4c85d0ec97abd25967d
Леонид <33185661+leonidsmirnov21@users.noreply.github.com>
2018-05-30 20:45:48 +0300

    rework mistakes

--- js/script.js
+++ js/script.js
@@ -1,23 +1,25 @@
-var fireballSize = 22;
+// 'use strict';
 
-var getFireballSpeed = function (left) {
-  if (left) {
-    return 5;
-  }
-  return 2;
-};
+// var fireballSize = 22;
 
-var wizardSpeed = 3;
-var wizardWidth = 70;
+// var getFireballSpeed = function (left) {
+//   if (left) {
+//     return 5;
+//   }
+//   return 2;
+// };
 
-var getWizardHeight = function () {
-  return 1.337 * wizardWidth;
-};
+// var wizardSpeed = 3;
+// var wizardWidth = 70;
 
-var getWizardX = function (width) {
-  return width / 2 - wizardWidth / 2;
-};
+// var getWizardHeight = function () {
+//   return 1.337 * wizardWidth;
+// };
 
-var getWizardY = function (height) {
-  return height / 3 * 2 - getWizardHeight();
-};
+// var getWizardX = function (width) {
+//   return width / 2 - wizardWidth / 2;
+// };
+
+// var getWizardY = function (height) {
+//   return height / 3 * 2 - getWizardHeight();
+// };
